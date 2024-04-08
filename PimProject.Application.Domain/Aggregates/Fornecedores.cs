47837536de6506b0a7f791545095844d30c6e65e
Kostylll <99488902+Kostylll@users.noreply.github.com>
2024-04-08 12:38:07 -0300

    Added Repository and Collections

--- PimProject.Application.Domain/Aggregates/Fornecedores.cs
+++ PimProject.Application.Domain/Aggregates/Fornecedores.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PimProject.Application.Abstraction.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace PimProject.Application.Domain.Aggregates
 {
-    internal class Fornecedores
+    public class Fornecedores : Entity
     {
     }
 }
