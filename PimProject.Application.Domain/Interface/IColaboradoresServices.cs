7ccef4435377f8c4e73791b2edace3a2099f60b0
Kostylll <99488902+Kostylll@users.noreply.github.com>
2024-04-11 12:25:51 -0300

    Register Function

--- PimProject.Application.Domain/Interface/IColaboradoresServices.cs
+++ PimProject.Application.Domain/Interface/IColaboradoresServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PimProject.Application.Domain.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace PimProject.Application.Domain.Interface
 {
     public interface IColaboradoresServices
     {
+        Task<bool> AdicionarColaborador(ColaboradoresResponse response);
+
+
+
     }
 }
--- PimProject.Application/Services/ColaboradoresService.cs
+++ PimProject.Application/Services/ColaboradoresService.cs
@@ -1,4 +1,8 @@
-﻿using PimProject.Application.Domain.Interface;
+﻿using PimProject.Application.Domain.Aggregates;
+using PimProject.Application.Domain.Interface;
+using PimProject.Application.Domain.Response;
+using PimProject.Application.Infra.Data.DBContext;
+using PimProject.Application.Infra.Data.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +13,39 @@ namespace PimProject.Application.Services
 {
     public class ColaboradoresService : IColaboradoresServices
     {
+        private readonly PimProjectDbContext _context;
+        private readonly IColaboradoresRepository _colRepository;
+
+        public ColaboradoresService()
+        {
+        }
+
+        public ColaboradoresService(IColaboradoresRepository colRepository, PimProjectDbContext context)
+        {
+            _context = context;
+            _colRepository = colRepository;
+        }
+
+        public async Task<bool> AdicionarColaborador(ColaboradoresResponse response)
+        {
+            var colab = new Colaboradores()
+            {
+                Nome = response.Nome,
+                Sobrenome = response.Sobrenome,
+                Email = response.Email,
+                Data_Nascimento = response.Data_Nascimento,
+                CPF = response.CPF,
+
+            };
+
+            await _colRepository.AddAsync(colab);
+            await _colRepository.SaveChangesAsync();
+
+
+
+            return true;
+        }
+
+
     }
 }
